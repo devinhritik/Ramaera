ac6ab9b29f33363eb368c13ec460327ce69639e5
Hritik chaudhary <ch.hritik98@gmail.com>
2023-01-18 23:55:54 +0530

    first commit

---
+++ src/pages/RamaeraLandingPage.js
@@ -0,0 +1,297 @@
+import styles from "./RamaeraLandingPage.module.css";
+
+const RamaeraLandingPage = () => {
+  return (
+    <div className={styles.ramaeraLandingPage}>
+      <img
+        className={styles.ramaeraLandingPageChild}
+        alt=""
+        src="../group-7.svg"
+      />
+      <img
+        className={styles.ramaeraLandingPageItem}
+        alt=""
+        src="../group-8.svg"
+      />
+      <div className={styles.service}>
+        <div className={styles.eCommarce}>
+          <img className={styles.linesIcon} alt="" src="../lines.svg" />
+          <button className={styles.button}>
+            <div className={styles.button1}>
+              <div className={styles.button2}>Read more</div>
+            </div>
+            <img
+              className={styles.buttonChild}
+              alt=""
+              src="../ellipse-101.svg"
+            />
+          </button>
+          <div className={styles.heading}>
+            <div className={styles.ramaeraSupermartIsContainer}>
+              <p className={styles.ramaeraSupermartIs}>
+                Ramaera Supermart is an online customer oriented marketplace to
+                motivated to provide everything you need right at one place
+                under the comfort of your homes.
+              </p>
+              <p className={styles.p}>{` `}</p>
+            </div>
+            <b className={styles.getAllYourContainer}>
+              <p className={styles.ramaeraSupermartIs}>
+                <span>{`Get all your essentials in `}</span>
+                <span className={styles.onePlace}>one place</span>
+              </p>
+            </b>
+          </div>
+          <img className={styles.icon} alt="" src="../icon.svg" />
+          <img
+            className={styles.ecomrceGifIcon}
+            alt=""
+            src="../ecomrce-gif@2x.png"
+          />
+        </div>
+        <div className={styles.agri}>
+          <img className={styles.linesIcon1} alt="" src="../lines1.svg" />
+          <div className={styles.button3}>
+            <div className={styles.button4}>
+              <div className={styles.button5}>Read more</div>
+            </div>
+            <img
+              className={styles.buttonChild}
+              alt=""
+              src="../ellipse-101.svg"
+            />
+          </div>
+          <img className={styles.icon1} alt="" src="../icon1.svg" />
+          <div className={styles.heading1}>
+            <div className={styles.ourAgriBusinessIndustryContainer}>
+              <p className={styles.ramaeraSupermartIs}>
+                Our Agri-business industry is established to get you fresh and
+                organic fruits and vegetables with the blessed with the touch of
+                nature
+              </p>
+            </div>
+            <b className={styles.agriBusinessToBestowContainer}>
+              <p className={styles.ramaeraSupermartIs}>
+                <span>Agri-business to bestow</span>
+                <span className={styles.naturesLove}> Nature's Love</span>
+              </p>
+            </b>
+          </div>
+          <img className={styles.agriGifIcon} alt="" src="../agri-gif@2x.png" />
+        </div>
+        <div className={styles.fmcg}>
+          <img className={styles.linesIcon2} alt="" src="../lines2.svg" />
+          <div className={styles.button6}>
+            <div className={styles.button7}>
+              <div className={styles.button5}>Read more</div>
+            </div>
+            <img
+              className={styles.buttonChild}
+              alt=""
+              src="../ellipse-1012.svg"
+            />
+          </div>
+          <div className={styles.heading2}>
+            <div className={styles.ramaeraFmcgIndustryContainer}>
+              <p className={styles.ramaeraSupermartIs}>
+                Ramaera FMCG industry has commenced an extensive range of FMCG
+                to make your days more than ordinary. We plan to offer you
+                nothing less than the best.
+              </p>
+            </div>
+            <b className={styles.offeringTheBestContainer}>
+              <p className={styles.ramaeraSupermartIs}>
+                Offering the best with FMCG industry
+              </p>
+            </b>
+          </div>
+          <img className={styles.icon2} alt="" src="../icon2.svg" />
+          <img className={styles.fmcgGifIcon} alt="" src="../fmcg-gif@2x.png" />
+        </div>
+        <div className={styles.packing}>
+          <div className={styles.heading3}>
+            <div className={styles.weProvidePackagingContainer}>
+              <p className={styles.ramaeraSupermartIs}>
+                We provide packaging solutions to help your business flourish
+                with boosted outreach. Ensuring the protection, containment, and
+                communication of packaged projects is our foremost priority.
+              </p>
+            </div>
+            <b className={styles.packagingIndustryForContainer}>
+              <p className={styles.ramaeraSupermartIs}>
+                <span>Packaging industry for business</span>
+                <span className={styles.growth}> growth</span>
+              </p>
+            </b>
+          </div>
+          <img className={styles.icon3} alt="" src="../icon3.svg" />
+          <img className={styles.linesIcon1} alt="" src="../lines3.svg" />
+          <div className={styles.button9}>
+            <div className={styles.button10}>
+              <div className={styles.readMore}>Read more</div>
+            </div>
+            <img
+              className={styles.buttonChild}
+              alt=""
+              src="../ellipse-1012.svg"
+            />
+          </div>
+          <img
+            className={styles.packingGifIcon}
+            alt=""
+            src="../packing-gif@2x.png"
+          />
+        </div>
+        <div className={styles.hotel}>
+          <img className={styles.linesIcon1} alt="" src="../lines4.svg" />
+          <div className={styles.button11}>
+            <div className={styles.button12}>
+              <div className={styles.button5}>Read more</div>
+            </div>
+            <img
+              className={styles.buttonChild}
+              alt=""
+              src="../ellipse-101.svg"
+            />
+          </div>
+          <div className={styles.heading4}>
+            <div className={styles.subHeading}>
+              <p className={styles.ramaeraSupermartIs}>
+                Introducing capsule hotels in places of extreme need such as
+                examination centres and hospitals to provide comfortable and
+                convenient accommodation at pocket friendly prices .
+              </p>
+            </div>
+            <b className={styles.heading5}>
+              <p className={styles.ramaeraSupermartIs}>
+                Making accommodations easier
+              </p>
+            </b>
+          </div>
+          <img className={styles.icon4} alt="" src="../icon4.svg" />
+        </div>
+        <div className={styles.technology}>
+          <img className={styles.linesIcon1} alt="" src="../lines5.svg" />
+          <div className={styles.button13}>
+            <div className={styles.button14}>
+              <div className={styles.button5}>Read more</div>
+            </div>
+            <img
+              className={styles.buttonChild}
+              alt=""
+              src="../ellipse-1015.svg"
+            />
+          </div>
+          <div className={styles.subHeading1}>
+            <p className={styles.ramaeraSupermartIs}>
+              We believe in innovation and excellence hence , our approach in
+              the field of technology is focused to relish incredible customer
+              experience and stabilising business models for tomorrow.
+            </p>
+          </div>
+          <b className={styles.mainHeading}>
+            <p className={styles.ramaeraSupermartIs}>
+              <span>Unleashing the strength of</span>
+              <span className={styles.technology1}> Technology</span>
+            </p>
+            <p className={styles.p}>
+              <span className={styles.technology1}>&nbsp;</span>
+            </p>
+          </b>
+          <img className={styles.icon5} alt="" src="../icon5.svg" />
+        </div>
+      </div>
+      <div className={styles.heading6}>
+        <div className={styles.subHeading2}>
+          <p className={styles.weAreAiming}>
+            We are aiming to be a part of the major industrialisation drive in
+            India and embarking our presence in every significant industrial
+            domain including IT.
+          </p>
+        </div>
+        <div className={styles.mainHeading1}>The Ramaera Expansion</div>
+      </div>
+      <div className={styles.heroSection}>
+        <img className={styles.bgIcon} alt="" src="../bg@2x.png" />
+        <div className={styles.heroSectionRight}>
+          <div className={styles.sliding}>
+            <div className={styles.div}>
+              <b className={styles.slide}>02</b>
+              <div className={styles.slide1} />
+              <div className={styles.slide2} />
+              <div className={styles.slide3} />
+            </div>
+          </div>
+        </div>
+        <div className={styles.heroSectionRightMedia}>
+          <img className={styles.vectorIcon} alt="" src="../vector.svg" />
+          <img className={styles.vectorIcon1} alt="" src="../vector1.svg" />
+          <img className={styles.vectorIcon2} alt="" src="../vector2.svg" />
+        </div>
+        <div className={styles.contentButton}>
+          <div className={styles.ramaeraIndustries}>Ramaera Industries</div>
+          <div className={styles.subHeading3}>
+            <p className={styles.anIncredibleMultiIndustrial}>
+              An incredible multi-industrial approach oriented towards financial
+              independence, customer focus and serving the best quality to the
+              people.
+            </p>
+          </div>
+          <div className={styles.heroSectionContent}>
+            <div className={styles.button16}>
+              <div className={styles.exploreMore}>Explore more</div>
+            </div>
+          </div>
+          <img className={styles.vectorIcon3} alt="" src="../vector3.svg" />
+          <div className={styles.workParent}>
+            <div className={styles.work}>WOrk</div>
+            <div className={styles.group}>
+              <div className={styles.s}>S</div>
+              <div className={styles.c}>C</div>
+              <div className={styles.r}>R</div>
+              <div className={styles.o}>O</div>
+              <div className={styles.l}>L</div>
+              <div className={styles.l1}>L</div>
+              <div className={styles.div1}> </div>
+              <div className={styles.t}>T</div>
+              <div className={styles.o1}>O</div>
+              <div className={styles.div2}> </div>
+              <div className={styles.e}>E</div>
+              <div className={styles.x}>X</div>
+              <div className={styles.p1}>P</div>
+              <div className={styles.l2}>L</div>
+              <div className={styles.o2}>O</div>
+              <div className={styles.r1}>R</div>
+              <div className={styles.e1}>E</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.header}>
+        <div className={styles.bg} />
+        <img className={styles.searchIcon} alt="" src="../search-icon.svg" />
+        <div className={styles.contact}>Contact</div>
+        <div className={styles.career}>Career</div>
+        <div className={styles.ourCompanies}>
+          <div className={styles.ourCompanies1}>Our companies</div>
+          <img className={styles.shapeIcon} alt="" src="../shape.svg" />
+        </div>
+        <div className={styles.ourIndustries}>
+          <div className={styles.ourIndustries1}>Our Industries</div>
+          <img className={styles.shapeIcon1} alt="" src="../shape.svg" />
+        </div>
+        <div className={styles.home}>
+          <div className={styles.home1}>Home</div>
+          <div className={styles.homeChild} />
+        </div>
+        <img
+          className={styles.logoRamaera1}
+          alt=""
+          src="../logo-ramaera-1@2x.png"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default RamaeraLandingPage;
